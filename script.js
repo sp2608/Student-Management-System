d68ce6d4778cc9411afd9708517975b09995ea8b
sp2608 <156405526+sp2608@users.noreply.github.com>
2025-02-13 11:51:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Function to show sections dynamically
+function showSection(sectionId) {
+    document.querySelectorAll('.section').forEach(section => {
+        section.classList.remove('active');
+    });
+    document.getElementById(sectionId).classList.add('active');
+}
